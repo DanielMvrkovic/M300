f829cbad50a89848475535ea473490b3890f1caa
DanielMvrkovic <100419879+DanielMvrkovic@users.noreply.github.com>
2022-03-23 11:10:01 +0100

    removed old html template, added upload of new html file on vagrant

--- lb2/apache_conf.sh
+++ lb2/apache_conf.sh
@@ -4,16 +4,3 @@ sudo ufw enable
 sudo ufw allow 80/tcp
 
 rm /var/www/html/index.html
-touch /var/www/html/index.html
-
-cat >>/var/www/html/index.html<<EOF
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>My Webserver</h1>
-<p>My Webserver</p>
-
-</body>
-</html>
-EOF
